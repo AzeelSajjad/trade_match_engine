17987b2db2c8fede701c9cd2394bdb5f904f5f07
Azeel Sajjad <aasajjad05@gmail.com>
2025-04-30 09:02:11 -0400

    m

---
+++ src/matching_engine.cpp
@@ -0,0 +1,14 @@
+#include "matching_engine.hpp"
+#include "order.hpp"
+#include "order_book.hpp"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+bool MatchingEngine::processOrder(const Order& order){
+    if(order.side == OrderSide::BUY){
+        const auto& orders = orderBook.getBestSell();
+    } else if (order.side == OrderSide::SELL){
+
+    }
+}
